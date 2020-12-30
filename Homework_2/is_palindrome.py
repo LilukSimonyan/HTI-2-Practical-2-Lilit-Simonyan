c8760aa0f08e924248b28d94e3641ea7ee129110
LilukSimonyan <lilith.simonyan@gmail.com>
2020-12-30 10:42:04 +0400

    Պալինդրոմի մեջ սխալ ունեմ

--- Homework_2/is_palindrome.py
+++ Homework_2/is_palindrome.py
@@ -1,22 +1,26 @@
 s = input('Enter the word: ')
-
-if len(s) == 1:
-    print('Yes')
-elif len(s) == 2:
-    if s[0] != s[1]:
-        print('No')
-    else:
+# էսքան հատվածն ավելորդ էր
+# if len(s) == 1:
+#     print('Yes')
+# elif len(s) == 2:
+#     if s[0] != s[1]:
+#         print('No')
+#     else:
+#         print('Yes')
+# else:
+first = 0
+last = len(s) - 1
+while first < last:
+    if s[first] == s[last]:
+        first = first + 1
+        last = last - 1
         print('Yes')
-else:
-    first = 0
-    last = len(s) - 1
-    while first < last:
-        if s[first] == s[last]:
-            first = first + 1
-            last = last - 1
-            print('Yes')
-            break
-        else:
-            print('No')
-            break
+    else:
+        print('No')
+        break
+        # ես գիտեմ, որ սխալ կա մեջը, s[first] == s[last] դեպքում
+        # բերում է, բոլոր ելքերը (և yes, և no, օրինակ abccda input-ի դեպքում),
+        # իսկ ինձ պետք է, որ եթե թեկուզ մեկ false կա տպի No
+        # կուղղեմ, բայց գիտեմ երբ :-)
+        # հուշում կանե՞ք
 

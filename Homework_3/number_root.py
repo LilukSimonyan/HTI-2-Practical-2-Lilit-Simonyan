12ec5386113419f27c49f3c7e9eaf18f37958c0c
LilukSimonyan <lilith.simonyan@gmail.com>
2021-01-19 00:34:23 +0400

    9-րդ տողի վրա >= եմ սարքել

--- Homework_3/number_root.py
+++ Homework_3/number_root.py
@@ -6,15 +6,16 @@ def intermediate(n): #78996
         sum = sum + int(root_str[i])
     return sum
 def root(n):
-    while intermediate(n) > 10:
+    while intermediate(n) >= 10:
         n = intermediate(n)
         sum = 0
         intermediate(n)
     else:
         print(intermediate(n))
 
-
 root(78996)
 root(10)
 root(8)
 root(37100005)
+root(55)
+root(0)

7a859646ecbfdfdc0f3b10baf9f9a0ccd62bdf02
LilukSimonyan <lilith.simonyan@gmail.com>
2021-01-19 23:15:23 +0400

    Recursive

--- Homework_3/number_root.py
+++ Homework_3/number_root.py
@@ -4,14 +4,10 @@ def intermediate(n):
 
     for i in range(len(root_str)):
         sum = sum + int(root_str[i])
-    return sum
-def root(n):
-    while intermediate(n) >= 10:
-        n = intermediate(n)
-        sum = 0
-        intermediate(n)
+    while sum >= 10:
+        return intermediate(sum)
     else:
-        return intermediate(n)
+        return sum
 
 n = int(input("Let's find a root of number: "))
-print(root(n))
+print(intermediate(n))

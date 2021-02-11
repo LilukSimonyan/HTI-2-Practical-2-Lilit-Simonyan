4f752794b0a7361b603891f03a10c9fe4be8dce0
LilukSimonyan <lilith.simonyan@gmail.com>
2021-02-11 15:39:08 +0400

    Ուղղում

--- Homework_6/number_guesser.py
+++ Homework_6/number_guesser.py
@@ -1,29 +1,17 @@
 def number_guesser(num):
     start = 0
     end = 1000
-    n = (start + end)//2
-    print(f'My guess number 1: {n}')
-    num = int(input())
-    i = 1
+    i = 0
     while num != 0:
         if num == 1:
-            i +=1
-            start = n
-            n = (start + end) // 2
-            if n == start:
-                return 'I couldn’t guess in 10 steps! This means you cheated!'
-            print(f'My guess number {i}: {n}')
-            num = int(input())
+            start = (start + end) // 2
         elif num == -1:
-            i += 1
-            end = n
-            n = (start + end) // 2
-            if n // 2 == start:
-                return 'I couldn’t guess in 10 steps! This means you cheated!'
-            print(f'My guess number {i}: {n}')
-            num = int(input())
-    return f'Your number is {n}', f'I guessed it in {i} steps!'
+            end = (start + end) // 2
+        if start == (start + end) // 2:
+            return 'I couldn’t guess in 10 steps! This means you cheated!'
+        i += 1
+        num = int(input(f'My guess number {i}: {(start + end) // 2}\nInput: '))
+    return f'Your number is {(start + end) // 2}, I guessed it in {i} steps!'
 
-print('Think of a number between 1 and 999. Input 0 once you’re ready to play.')
-first_num = input()
+first_num = input(f'Think of a number between 1 and 999. Input 0 once you’re ready to play.\nInput: ')
 print(number_guesser(first_num))

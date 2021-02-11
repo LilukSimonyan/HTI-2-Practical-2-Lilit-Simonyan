ba7c5a94c358ff287d67ed0ec67eeb8f49e50f7d
LilukSimonyan <lilith.simonyan@gmail.com>
2021-02-11 18:55:34 +0400

    Hangman

--- Homework_6/hangman.py
+++ Homework_6/hangman.py
@@ -8,10 +8,10 @@ def get_a_random_word():
 
     with open('fruits.txt') as f:
         fruits = [row.strip() for row in f.readlines()]
-        return random.choices(fruits)
+        return random.choice(fruits)
 
-word = get_a_random_word()[0]
-# print(word)
+word = get_a_random_word()
+print(word)
 
 def game():
     l = len(word)
@@ -23,11 +23,13 @@ def game():
             return f'{"".join(guess)}\nYou won the game.'
         else:
             letter = input(f'Guess the word. {step} mistakes left.\n{"".join(guess)}\nGuess a letter: ').lower()
-            for i in range(l):
-                if letter == word[i]:
-                    guess[i] = (word[i]+" ").upper()
-                else:
-                    step -= 1
+            if letter in word:
+                for i in range(l):
+                    if letter == word[i]:
+                        guess[i] = (word[i]+" ").upper()
+
+            else:
+                step -= 1
     else:
         return "You lost the game."
 

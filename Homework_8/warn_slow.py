eb2c9b9e6f2dcfbcd9195a2d91805705599a2b38
LilukSimonyan <lilith.simonyan@gmail.com>
2021-03-04 00:00:23 +0400

    warn_slow_

---
+++ Homework_8/warn_slow.py
@@ -0,0 +1,34 @@
+import time
+import sys
+
+def warn_slow(func):
+    def inner(*args, **kwargs):
+        start = time.time()
+        result = func(*args)
+        duration = time.time() - start
+        if duration > 2:
+            print(f'It took {duration} seconds, {func.__name__}') #{արգումենտի համար չգտա ինչպես ստանալ}
+        return result
+    return inner
+
+@warn_slow
+def func_slow(n):
+    time.sleep(3)
+    result = ' *'
+    for i in range(n):
+        print('  ' * (n - i - 1) + result * (i + 1), end='')
+        print('')
+    for i in range(n - 1):
+        print('  ' * (i + 1) + result * (n - i - 1), end=' ')
+        print('')
+
+
+
+@warn_slow
+def func_fast(x, y, z):
+    print(f'Result of func_fast function is {x*y}')
+
+func_slow(5)
+print()
+func_fast(10, 15, 20)
+

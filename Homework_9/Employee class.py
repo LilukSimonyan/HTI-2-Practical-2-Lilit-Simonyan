7b639dac9eba4c1dbbcbb04045adc448dead11c6
LilukSimonyan <lilith.simonyan@gmail.com>
2021-03-10 23:07:43 +0400

    Employee

--- Homework_9/Employee class.py
+++ Homework_9/Employee class.py
@@ -3,11 +3,11 @@ import datetime
 
 
 class Employee:
-    def __init__(self, fisrt_name, last_name, trial_passed=False, gender='M'):
+    def __init__(self, fisrt_name, last_name, trial_passed=False):
         self.first_name = fisrt_name
         self.last_name = last_name
-        self.trial_passed = trial_passed
-        self.gender = gender
+        self._trial_passed = trial_passed
+
 
     @property
     def full_name(self):
@@ -20,69 +20,109 @@ class Employee:
             raise ValueError('Not allowed value.')
 
     @property
-    def phone(self):
-        return self.phone
+    def phone_number(self):
+        return self.phone_number
 
-    @phone.setter
-    def phone(self, phone_number):
-        def validate_phone_number(phone_number):
+    @phone_number.setter
+    def phone_number(self, _phone_number):
+        def validate_phone_number(_phone_number):
             exp = (
-                r'0?(77|55|99|91|98)[ -]?'  # operator code
+                r'0?(77|55|99|91|98)[ -]?'
                 '('
-                r'(\d{3}[ -]?\d{3})|'  # version 1
-                r'(\d{2} \d{2} \d{2})|'  # version 2
-                r'(\d{2}-\d{2}-\d{2})'  # version 3
+                r'(\d{3}[ -]?\d{3})|'
+                r'(\d{2} \d{2} \d{2})|'
+                r'(\d{2}-\d{2}-\d{2})'
                 ')'
             )
-            return re.fullmatch(exp, phone_number) is not None
-        if validate_phone_number(phone_number):
-            self.phone_number = phone_number
+            return re.fullmatch(exp, _phone_number) is not None
+        if validate_phone_number(_phone_number):
+            self._phone_number = _phone_number
         else:
-            raise TypeError('UNSUPPORTED PHONE_NUMBER')  # էլ. հասցեի դեպքում ստուգումն անում է, այս դեպքում՝ ոչ
+            raise TypeError('UNSUPPORTED PHONE_NUMBER')
 
     @property
     def work_email(self):
         return f'{self.first_name}.{self.last_name}@company.com'.lower()
 
     @work_email.setter
-    def work_email(self, email):
-        def validate_email(email):
+    def work_email(self, _work_email):
+        def validate_email(_work_email):
             exp = r'[a-z0-9]{1}(((\.?[\+\-\w])+[a-z0-9])?)@[a-z0-9]{1}(\.?[\+\-\w])*(\.[a-z]{2,10})'
-            return re.fullmatch(exp, email) is not None
-        if validate_email(email):
-            self.email = email
+            return re.fullmatch(exp, _work_email) is not None
+        if validate_email(_work_email):
+            self._work_email = _work_email
         else:
             raise TypeError('UNSUPPORTED EMAIL')
 
     @property
-    def start_date(self):
-        return self.start_date
+    def trial_passed(self):
+        return self._trial_passed
 
-    @start_date.setter
-    def start_date(self, join_date):
-        d = [int(elem) for elem in join_date.split('.')]
-        self.join_date = datetime.date(d[2], d[1], d[0])
-
-    @property
-    def end_date(self):
-        return self.end_date
-
-    @end_date.setter
-    def end_date(self, leave_date):
-        d = [int(elem) for elem in leave_date.split('.')]
-        self.leave_date = datetime.date(d[2], d[1], d[0])
+    @trial_passed.setter
+    def trial_passed(self, _trial_passed):
+        if type(_trial_passed) != bool:
+            raise ValueError(f'Trial_passed must be True or False, not {type(_trial_passed)}')
+        else:
+            self._trial_passed = _trial_passed
 
     @property
-    def salary_func(self):
-        return self.salary_func
+    def join_date(self):
+        return self.join_date
 
-    @salary_func.setter
-    def salary_func(self, salary):
-        self.salary = salary
+    @join_date.setter
+    def join_date(self, _join_date):
+        d = [int(elem) for elem in _join_date.split('.')]
+        self._join_date = datetime.date(d[2], d[1], d[0])
+
+    @property
+    def leave_date(self):
+        return self.leave_date
+
+    @leave_date.setter
+    def leave_date(self, _leave_date):
+        d = [int(elem) for elem in _leave_date.split('.')]
+        self._leave_date = datetime.date(d[2], d[1], d[0])
+
+    @property
+    def salary(self):
+        return self.salary
+
+    @salary.setter
+    def salary(self, _salary):
+        if not (type(_salary) == int or type(_salary) == float):
+            raise TypeError(f'Salary must be int or float, not {type(_salary)}')
+        else:
+            self._salary = _salary
+
+    @property
+    def gender(self):
+        return self.gender
+
+    @gender.setter
+    def gender(self, _gender):
+        if (_gender == 'M') | (_gender == 'F'):
+            self._gender = _gender
+        else:
+            raise TypeError("PLEASE, INPUT 'M' or 'F'")
 
     def __repr__(self):
-        return f'Beneficiary: {self.first_name} {self.last_name}'
+        return f'Employee: {self.first_name} {self.last_name}'
 
 
+emp1 = Employee('Lilit', 'Simonyan')
+emp1.work_email = 'lilit.simonyan@gmail.com'
+print(emp1.full_name)
+# print(emp1.work_email)
+# print(emp1.work_email)
+emp1.join_date = '01.02.2009'
+print(emp1._join_date)
+# emp1.trial_passed = None
+# print(emp1._trial_passed)
+# emp1.phone_number = '077 34 12 04'
+# print(emp1._phone_number)
+# emp1.salary = 150000
+# print(emp1._salary)
+# emp1.gender = 'K'
+# print(emp1._gender)
 
 
